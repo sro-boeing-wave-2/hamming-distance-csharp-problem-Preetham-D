5bbceac1fcfc233e8a6193a9a8f17c5b85f6ecad
Preetham D <preethamdbangera@gmail.com>
2018-07-19 11:44:47 +0530

    Exception handling

--- Hamming/Hamming.cs
+++ Hamming/Hamming.cs
@@ -7,8 +7,33 @@ namespace Hamming
         public static int Distance(string original, string current)
         {
             int hammingDistance = 0;
-            
-            return hammingDistance;
+            int i;
+            if (original == null)
+            {
+                throw new ArgumentNullException("original");
+            }
+            else if (current == null)
+            {
+                throw new ArgumentNullException("current");
+            }
+            else if (original.Length != current.Length)
+            {
+                throw new ArgumentException("Hamming Distance can only be calculated over strings of equal length");
+            }
+            else
+            {
+                for (i = 0; i < original.Length; i += 1)
+                {
+
+                    if (original[i] != current[i])
+                    {
+                        hammingDistance += 1;
+                    }
+
+                }
+            }
+           
+                        return hammingDistance;
         }
     }
 }
